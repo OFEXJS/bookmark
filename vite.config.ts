eb658252a24495122f59ba81cd16820e622aec36
zhengjialu8507 <zhengjialu8507@gmail.com>
2025-12-29 20:02:25 +0800

    添加代码混淆

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import obfuscator from "vite-plugin-bundle-obfuscator";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    obfuscator({
+      apply: "build", // 只在构建时应用
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: false,
+        debugProtection: false,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: "hexadecimal",
+        stringArray: true,
+        stringArrayEncoding: ["base64"],
+        stringArrayThreshold: 0.8,
+        transformObjectKeys: true,
+      },
+    }),
+  ],
+});
